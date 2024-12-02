861fb52d422d33df745d4e00a06d6a2dadc73fcd
kiper <permakovkirill925@gmail.com>
2024-12-02 22:26:33 +0500

    изменил путь

--- server.ts
+++ server.ts
@@ -2,11 +2,13 @@ import 'dotenv/config'
 import app from './src/app.js'
 import https from 'https'
 import fs from 'fs'
+import path from 'path'
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 const httpsOptions = {
-    key: fs.readFileSync('/path/to/server.key'), 
-    cert: fs.readFileSync('/path/to/server.cert') 
+    key: fs.readFileSync(`${__dirname}/server.key`), 
+    cert: fs.readFileSync(`${__dirname}/server.cert`) 
 };
 
 async function startApp() {
