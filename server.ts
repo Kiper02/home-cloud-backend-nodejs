f2d98b44308829a260cd2959558b811fe62eae42
kiper <permakovkirill925@gmail.com>
2024-12-02 22:24:01 +0500

    перешел на https

--- server.ts
+++ server.ts
@@ -1,12 +1,18 @@
 import 'dotenv/config'
 import app from './src/app.js'
+import https from 'https'
+import fs from 'fs'
 
 const PORT = process.env.PORT || 5000;
+const httpsOptions = {
+    key: fs.readFileSync('/path/to/server.key'), 
+    cert: fs.readFileSync('/path/to/server.cert') 
+};
 
 async function startApp() {
   try {
-    app.listen(PORT, () => {
-      console.log(`Server started on PORT = ${PORT}`);
+    https.createServer(httpsOptions, app).listen(443, () => {
+        console.log('HTTPS Server running on port 443');
     });
   } catch (e) {
     console.log(e);
